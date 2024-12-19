52fee5bb9bc732879cdf6703262249bff9038405
NaniT55 <121422688+NaniT55@users.noreply.github.com>
2024-12-19 15:00:43 +0530

    Update groomingfaqs.tsx

--- main-file/Data/groomingfaqs.tsx
+++ main-file/Data/groomingfaqs.tsx
@@ -37,51 +37,51 @@ export const groomingFaqData: FaqType[] = [
     _id: "1",
     color: "red",
     question: "What is ORN-AI Grooming?",
-    answer: [
-      {
-        text: "ORN-AI grooming is a tailored process designed to enhance crucial workplace skills, preparing you to excel in a competitive job market.",
-      },
-    ],
+    answer: 
+      
+        "ORN-AI grooming is a tailored process designed to enhance crucial workplace skills, preparing you to excel in a competitive job market.",
+      
+    
   },
   {
     _id: "2",
     color: "red",
     question: "Why Choose ORN-AI Grooming?",
-    answer: [
-      {
-        text: "Our plans transform your professional persona, equipping you with advanced skills in communication, presentation, and technical expertise for today's dynamic challenges.",
-      },
-    ],
+    answer: 
+      
+        "Our plans transform your professional persona, equipping you with advanced skills in communication, presentation, and technical expertise for today's dynamic challenges.",
+  
   },
   {
     _id: "3",
     color: "red",
     question: "How Does Grooming Work at ORN-AI?",
-    answer: [
-      {
-        text: "Start with a free assessment to identify your needs. We then customize a grooming plan that includes:",
-      },
-      {
-        text: "Enhanced Communication: Sharpen your business communication and telephone skills.",
-      },
-      {
-        text: "Language Mastery: Expand your English proficiency and grammar.",
-      },
-      {
-        text: "Professional Writing Development: Command respect with superior writing and proposal skills.",
-      },
-      {
-        text: "Presentation Excellence: Boost your public speaking and presentation capabilities.",
-      },
-      {
-        text: "Technological Proficiency: Update your skills with the latest digital tools.",
-      },
-      {
-        text: "Career Advancement: Get tailored interview prep, CV branding, and strategic placement.",
-      },
-      {
-        text: "Hands-on Experience: Participate in internships and receive targeted placement assistance.",
-      },
-    ],
+    answer: 
+      "Start with a free assessment to identify your needs. We then customize a grooming plan that includes:"
+      // {
+      //   text: "Start with a free assessment to identify your needs. We then customize a grooming plan that includes:",
+      // },
+      // {
+      //   text: "Enhanced Communication: Sharpen your business communication and telephone skills.",
+      // },
+      // {
+      //   text: "Language Mastery: Expand your English proficiency and grammar.",
+      // },
+      // {
+      //   text: "Professional Writing Development: Command respect with superior writing and proposal skills.",
+      // },
+      // {
+      //   text: "Presentation Excellence: Boost your public speaking and presentation capabilities.",
+      // },
+      // {
+      //   text: "Technological Proficiency: Update your skills with the latest digital tools.",
+      // },
+      // {
+      //   text: "Career Advancement: Get tailored interview prep, CV branding, and strategic placement.",
+      // },
+      // {
+      //   text: "Hands-on Experience: Participate in internships and receive targeted placement assistance.",
+      // },
+    
   },
 ];
